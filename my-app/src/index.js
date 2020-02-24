af6acced1a33f59e64e51d02dfea1ae128747832
caoimhe <caoimhe.gallahue@gmail.com>
2020-02-23 18:53:18 -0800

    added small insta logo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -46,6 +46,9 @@ class Info extends React.Component{
                 game on Saturday <br/>
                 February 8th!</h3>
             <img src='/PlaceHolderImage.jpg' alt='Team of the week'></img>
+            <a href="https://instagram.com">
+            <img src='/instagram.png' class='instagramIcon' alt='Instagram'></img>
+            </a>
         </div>
     }
 }
