97627b95726dc4a93f7213db9d948c7a8d9989b7
adrien-chaussabel <adrien.chaussabel@gmail.com>
2020-04-22 18:54:04 -0700

    continued playing around with bootstrap

--- my-app/src/components/home page/bootstrapMenu.jsx
+++ my-app/src/components/home page/bootstrapMenu.jsx
@@ -4,7 +4,9 @@ import { Navbar, Nav } from 'react-bootstrap';
 const styles = {
   menu: {
     backgroundColor: '#154734',
-    textcolor: '#C9CAC7',
+  },
+  links: {
+    color: '#C9CAC7',
   },
 };
 
@@ -12,7 +14,7 @@ const MenuBar = () => (
   <>
     <Navbar style={styles.menu}>
       <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="justify-content-end">
+      <Nav className="ml-auto" style={styles.links}>
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
 
+// Comment out when revert
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
