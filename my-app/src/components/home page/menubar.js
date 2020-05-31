49944e30f72b1eb60f44aa2a6bb283453d524ea0
Luis <luisarmendariz99@gmail.com>
2020-05-31 02:17:41 -0700

    after merge

--- my-app/src/components/home page/menubar.js
+++ my-app/src/components/home page/menubar.js
@@ -19,6 +19,7 @@ import FormsPage from '../forms/forms';
 import SportsPage from '../sports/sports';
 import Gallery from './carouselHome';
 import NewEvent from '../events/newEvent';
+import CalendarPage from '../calendar/calendar'
 
 
 class HomePage extends React.Component {
@@ -35,31 +36,74 @@ class HomePage extends React.Component {
 }
 
 
-/* TODO: add link to sports page on menu bar */
 class Menu extends React.Component {
   render() {
-    return (
-      <Router>
-        <div className="menu">
-          <img src="/CalPolyLogo.png" alt="Cal Poly Logo" />
-          <h1>CLUB SPORTS HUB</h1>
-          <Link to="/login">LOGIN</Link>
-          <Link to="/forms">FORMS</Link>
-          <SportsDropDown />
-          <Link to="calendar">CALENDAR</Link>
-          <Link to="/">HOME</Link>
-        </div>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/forms" component={FormsPage} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/newEvent" component={NewEvent} />
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
+      return <Router>
+          <div className="menu">
+              <img src='/CalPolyLogo.png' alt='Cal Poly Logo' />
+              <h1>CLUB SPORTS HUB</h1>
+              <Link to="/login">LOGIN</Link>
+              <Link to="/forms">FORMS</Link>
+              <div class="dropdown">
+          <button class="dropbtn">SPORTS
+              <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+              <div class="row">
+                  <div class="column">
+                      <h3>MEN'S</h3>
+                      <h4>__________</h4>
+                      <Link to="/sports">BASKETBALL</Link>
+                      <Link to="/sports">LACROSSE</Link>
+                      <Link to="/sports">HOCKEY</Link>
+                      <Link to="/sports">RUGBY</Link>
+                      <Link to="/sports">SOCCER</Link>
+                      <Link to="/sports">ULTIMATE</Link>
+                      <Link to="/sports">VOLLEYBALL</Link>
+                      <Link to="/sports">WATER POLO</Link>
+                  </div>
+                  <div class="column">
+                      <h3>WOMEN'S </h3>
+                      <h4>__________</h4>
+                      <Link to="/sports">BASKETBALL</Link>
+                      <Link to="/sports">RUGBY</Link>
+                      <Link to="/sports">SOCCER</Link>
+                      <Link to="/sports">ULTIMATE</Link>
+                      <Link to="/sports">VOLLEYBALL</Link>
+                      <Link to="/sports">WATER POLO</Link>
+                  </div>
+                  <div class="column">
+                      <h3>COED</h3>
+                      <h4>__________</h4>
+                      <Link to="/sports">CYCLING</Link>
+                      <Link to="/sports">BOWLING</Link>
+                      <Link to="/sports">DISTANCE</Link>
+                      <Link to="/sports">FENCING</Link>
+                      <Link to="/sports">SAILING</Link>
+                      <Link to="/sports">SURF</Link>
+                      <Link to="/sports">SWIM</Link>
+                      <Link to="/sports">TENNIS</Link>
+                      <Link to="/sports">TRIATHLON</Link>
+                      <Link to="/sports">WATER SKI</Link>
+                  </div>
+              </div>
+          </div>
+      </div>
+              <Link to="calendar">CALENDAR</Link>
+              <Link to="/">HOME</Link>
+          </div>;
+
+          <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/forms" component={FormsPage} />
+              <Route exact path="/register" component={Register} />
+              <Route path="/sports" component={SportsPage} />
+              <Route path="/calendar" component={CalendarPage} />
+              <Route path="/login">
+                  <Login />;
+              </Route>
+          </Switch>
       </Router>
-    );
   }
 }
 
@@ -77,38 +121,38 @@ class SportsDropDown extends React.Component {
               <div className="column">
                 <h3>MEN&apos;S</h3>
                 <h4>__________</h4>
-                <b href="#">BASKETBALL</b>
-                <b href="#">LACROSSE</b>
-                <b href="#">HOCKEY</b>
-                <b href="#">RUGBY</b>
-                <b href="#">SOCCER</b>
-                <b href="#">ULTIMATE</b>
-                <b href="#">VOLLEYBALL</b>
-                <b href="#">WATER POLO</b>
+                <Link to="/sports">BASKETBALL</Link>
+                <Link to="/sports">LACROSSE</Link>
+                <Link to="/sports">HOCKEY</Link>
+                <Link to="/sports">RUGBY</Link>
+                <Link to="/sports">SOCCER</Link>
+                <Link to="/sports">ULTIMATE</Link>
+                <Link to="/sports">VOLLEYBALL</Link>
+                <Link to="/sports">WATER POLO</Link>
               </div>
               <div className="column">
                 <h3>WOMEN&apos;S </h3>
                 <h4>__________</h4>
-                <b href="#">BASKETBALL</b>
-                <b href="#">RUGBY</b>
-                <b href="#">SOCCER</b>
-                <b href="#">ULTIMATE</b>
-                <b href="#">VOLLEYBALL</b>
-                <b href="#">WATER POLO</b>
+                <Link to="/sports">BASKETBALL</Link>
+                <Link to="/sports">RUGBY</Link>
+                <Link to="/sports">SOCCER</Link>
+                <Link to="/sports">ULTIMATE</Link>
+                <Link to="/sports">VOLLEYBALL</Link>
+                <Link to="/sports">WATER POLO</Link>
               </div>
               <div className="column">
                 <h3>COED</h3>
                 <h4>__________</h4>
-                <b href="#">CYCLING</b>
-                <b href="#">BOWLING</b>
-                <b href="#">DISTANCE</b>
-                <b href="#">FENCING</b>
-                <b href="#">SAILING</b>
-                <b href="#">SURF</b>
-                <b href="#">SWIM</b>
-                <b href="#">TENNIS</b>
-                <b href="#">TRIATHLON</b>
-                <b href="#">WATER SKI</b>
+                <Link to="/sports">CYCLING</Link>
+                <Link to="/sports">BOWLING</Link>
+                <Link to="/sports">DISTANCE</Link>
+                <Link to="/sports">FENCING</Link>
+                <Link to="/sports">SAILING</Link>
+                <Link to="/sports">SURF</Link>
+                <Link to="/sports">SWIM</Link>
+                <Link to="/sports">TENNIS</Link>
+                <Link to="/sports">TRIATHLON</Link>
+                <Link to="/sports">WATER SKI</Link>
               </div>
             </div>
           </div>
